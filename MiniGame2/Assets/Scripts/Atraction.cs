8bb46f30ffb675a9d0935f569e0dd88bdf6ef311
Vladislav Bulantsev <vladislavbv96@gmail.com>
2018-09-27 12:25:39 +0200

    ball and cilinder attraction

---
+++ MiniGame2/Assets/Scripts/Atraction.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atraction : MonoBehaviour {
+
+    // Use this for initialization
+
+    public GameObject attractedTo;
+    private Rigidbody rb;
+
+    public float strengthOfAttraction;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 direction = attractedTo.transform.position - transform.position;
+
+        rb.AddForce(strengthOfAttraction * direction);
+	}
+}
